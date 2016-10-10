bdd41efa19dd709cfc121416fe5081e325b44c6e
ar-naseef <naseef14@gmail.com>
2016-10-10 11:58:21 +0530

    ajax added.

--- index.js
+++ index.js
@@ -24,7 +24,90 @@ $(window).scroll(function(){
 //js to show apod
 //var show_apod_btn = document.getElementById("show_apod_btn");
 
-function show_apod () {
+var json_array = [];
+
+function showApod() {
+    var apod_title = document.getElementById("apod_title");
+    apod_title.innerHTML = "<h2>LOADING...</h2>";
+    getContents();
     var apod_container = document.getElementById("apod_container");
     apod_container.style.display = "block";
 }
+
+function getContents() {
+
+    var xmlreq = new XMLHttpRequest();
+    xmlreq.open("GET", "https://api.nasa.gov/planetary/apod?api_key=EeJZi2jrsbbR96g169oVfq4f788ZrUEQBy9hu27R", true);
+    //&date=2016-10-09
+    
+    xmlreq.responseText = 'text';
+    
+    xmlreq.onreadystatechange = function () {
+        //request is done?
+        if (xmlreq.readyState === xmlreq.DONE) {
+            //if the response is 200
+            if (xmlreq.status === 200) {
+                //process data
+                
+                var raw_res = xmlreq.responseText;
+                json_array = JSON.parse(raw_res);
+                //console.log(json_array);
+                if (json_array["media_type"] == "image" ) {
+                    //set title
+                    var apod_title = document.getElementById("apod_title");
+                    apod_title.innerHTML = json_array["title"];
+
+                    //set img
+                    var apod_img = document.getElementById("apod_img");
+                    apod_img.style.display = "inherit";
+                    apod_img.src = json_array["url"];
+
+                    //ser brief exp
+                    var apod_brief = document.getElementById("apod_brief");
+                    apod_brief.innerHTML = json_array["explanation"];
+
+                    //set date
+                    var apod_date = document.getElementById("apod_date");
+                    apod_date.innerHTML = "date : " + json_array["date"];
+                } else {
+                    //set title
+                    var apod_title = document.getElementById("apod_title");
+                    apod_title.innerHTML = json_array["title"] + "<br/><br/><br/><h4>media doesn't support..</h4>";
+
+//                    //set img
+//                    var apod_img = document.getElementById("apod_img");
+//                    apod_img.style.display = "inherit";
+//                    apod_img.src = json_array["url"];
+                    
+
+                    //ser brief exp
+                    var apod_brief = document.getElementById("apod_brief");
+                    apod_brief.innerHTML = json_array["explanation"];
+
+                    //set date
+                    var apod_date = document.getElementById("apod_date");
+                    apod_date.innerHTML = "date : " + json_array["date"];
+                }
+                
+            } else {
+                //something went wrong
+                //check internet connection
+                var apod_title = document.getElementById("apod_title");
+                apod_title.innerHTML = "<h3>check your internet connection..!</h3>";
+            }
+        } else {
+            //loading
+            console.log("loading");
+        }
+    };
+    
+    xmlreq.send(null);
+
+}
+
+
+
+
+
+
+
